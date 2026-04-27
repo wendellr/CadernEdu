e8ceb1fc0a09c8332ada5f5b2d7f11c5a3652399
Wendell Rodrigues <wendell.oliveira@nepen.org.br>
2026-04-26 22:38:58 -0300

    changes: next gera server

--- apps/web_painel/next.config.js
+++ apps/web_painel/next.config.js
@@ -1,12 +1,7 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   transpilePackages: ['@cadernedu/api-client'],
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 }
 
 module.exports = nextConfig
--- apps/web_site/next.config.js
+++ apps/web_site/next.config.js
@@ -1,13 +1,6 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Garante que o tracing de arquivos inclua o monorepo root
-  // (necessário para importar packages/* no Docker)
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 }
 
 module.exports = nextConfig
