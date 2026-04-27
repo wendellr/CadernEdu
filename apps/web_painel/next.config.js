6fd1867b298f2e81370f404f925992b58195f7ef
Wendell Rodrigues <wendell.oliveira@nepen.org.br>
2026-04-26 22:45:18 -0300

    fix: server.js

--- apps/web_painel/next.config.js
+++ apps/web_painel/next.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   transpilePackages: ['@cadernedu/api-client'],
 }
 
